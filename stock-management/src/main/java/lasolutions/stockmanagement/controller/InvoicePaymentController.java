de341fd666647d3f26217a4a51d64e289f87de7e
Hunridolla <macintoshhd@Macintoshs-MacBook-Pro.local>
2020-04-13 22:40:58 +0700

    Monday

--- stock-management/src/main/java/lasolutions/stockmanagement/controller/InvoicePaymentController.java
+++ stock-management/src/main/java/lasolutions/stockmanagement/controller/InvoicePaymentController.java
@@ -1,5 +1,6 @@
 package lasolutions.stockmanagement.controller;
 
+import lasolutions.stockmanagement.Customer.CustomerRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InvoicePaymentController {
+    private final CustomerRepository customerRepository;
+
+    public InvoicePaymentController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @RequestMapping(value = "/invoice-payments", method = RequestMethod.GET)
     public ModelAndView showInvoicePayments(){
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("customers",customerRepository.getCustomers());
         modelAndView.setViewName("invoice-payments");
         return modelAndView;
     }
